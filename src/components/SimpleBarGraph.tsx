0831d66c29ec1ae3ddd8fa72b63ca8ac83c81d8b
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-22 11:08:57 -0400

    created component skeleton

---
+++ src/components/SimpleBarGraph.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import visx from '@visx/visx'
+import { Button } from '@material-ui/core'
+
+export const SimpleBarGraph: React.FC = () => (
+  <>
+    <p>Simple Bar Graph</p>
+    <Button>Button 1</Button>
+    <Button>Button 2</Button>
+  </>
+)
