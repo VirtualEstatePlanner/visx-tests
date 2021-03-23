9579e875ccfdb6159e9115bc509db63ae9d3ca74
VirtualEstatePlanner <georgegeorgulas@gmail.com>
2021-03-22 22:36:12 -0400

    standardized component sizes

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import { ThemeProvider } from '@material-ui/core'
 import { DrawingBoard } from './components/DrawingBoard/DrawingBoard'
 import { Zoomer } from './components/Zoomer/Zoomer'
 
+const dimensions: number[] = [720, 480]
+
 export const App: React.FC = () => {
   return (
     <div className='App'>
       <ThemeProvider theme={AsapTheme}>
         <p>
           DrawingBoard component
-          <DrawingBoard data={[]} width={1080} height={720} />
+          <DrawingBoard data={[]} width={dimensions[0]} height={dimensions[1]} />
         </p>
         <p>
           Zoomer component
-          <Zoomer width={1080} height={720} />
+          <Zoomer width={dimensions[0]} height={dimensions[1]} />
         </p>
       </ThemeProvider>
     </div>
